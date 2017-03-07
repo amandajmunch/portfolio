8940c736a4f92b6b6ef86976bd7bac8f03f96841
Amanda Munch <amandajmunch@gmail.com>
2017-03-07 08:56:31 -0500

    updated references

--- style.js
+++ style.js
@@ -1,12 +1,8 @@
 $(document).ready(() =>{
 
-// window.scrollBy({
-//   top: 100, // could be negative value
-//   left: 0,
-//   behavior: 'smooth'
-// });
 
 
+// https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -22,72 +18,27 @@ $(function() {
   });
 });
 
+// http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
+//  var sections = $('section')
+//   , nav = $('nav')
+//   , nav_height = nav.outerHeight();
 
+// $(window).on('scroll', function () {
+//   var cur_pos = $(this).scrollTop();
 
+//   sections.each(function() {
+//     var top = $(this).offset().top - nav_height,
+//         bottom = top + $(this).outerHeight();
 
+//     if (cur_pos >= top && cur_pos <= bottom) {
+//       nav.find('a').removeClass('active');
+//       sections.removeClass('active');
 
-// $(document).on('scroll', function() {
-//       var currentPosition = $(this).scrollTop();
-//       $('.section').each(function() {
-//         var sectionPosition = $(this).offset().top;
-//         if(sectionPosition < currentPosition) {
-//           $('a').removeClass('active');
-//           $('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-//         }
-//       });
-//     });
-//     $('a').on('click', function() {
-//       $('a').removeClass('active');
 //       $(this).addClass('active');
-//     });
-
-// $(document).scroll(function() {
-//   $('nav a[href="#'+$('section:hover').attr('id')+'"]').addClass('active').siblings('nav a').removeClass('active');
+//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+//     }
+//   });
 // });
-
-
-
-// $(document).scroll(function(){
-//         positionNav('name', 'name');
-//         positionNav('about', 'about');
-//         positionNav('projects', 'projects');
-//         positionNav('skills', 'skills');
-//         positionNav('contact', 'contact');
-// });
-
- //    function positionNav(section, sectionClass) {
- //        // Get bottom position of section
- //        var bottom = $('#' + section).position().top + $('#' + section).outerHeight(true);
- //        var top = $(document).scrollTop()+230;
-
- //        if(top >= $('#' + section).position().top && top <= bottom){
- //            $('#' + sectionClass).addClass('active');
- //        } else {
- //            $('#' + sectionClass).removeClass('active');
- //        }
- // };
-
-
- var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
-
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
-
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
-
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
-});
 
 
 
