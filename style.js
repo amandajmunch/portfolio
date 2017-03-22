c926a0496787357d1a325edf3e5fca5f533074be
Amanda Munch <amandajmunch@gmail.com>
2017-03-21 23:11:37 -0400

    css and add project

--- style.js
+++ style.js
@@ -1,52 +1,37 @@
 $(document).ready(() =>{
 
-
-
-// https://css-tricks.com/snippets/jquery/smooth-scrolling/
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
+  $('.about').click(function(){
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: $('#about').offset().top - 20
         }, 1000);
-        return false;
-      }
-    }
-  });
-});
+      });
 
-// http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
-//  var sections = $('section')
-//   , nav = $('nav')
-//   , nav_height = nav.outerHeight();
+  $('.projects').click(function(){
+        $('html, body').animate({
+          scrollTop: $('#projects').offset().top - 20
+        }, 1000);
+      });
 
-// $(window).on('scroll', function () {
-//   var cur_pos = $(this).scrollTop();
+  $('.skills').click(function(){
+        $('html, body').animate({
+          scrollTop: $('#skills').offset().top - 20
+        }, 1000);
+      });
 
-//   sections.each(function() {
-//     var top = $(this).offset().top - nav_height,
-//         bottom = top + $(this).outerHeight();
+  $('.contact').click(function(){
+        $('html, body').animate({
+          scrollTop: $('#contact').offset().top - 20
+        }, 1000);
+      });
 
-//     if (cur_pos >= top && cur_pos <= bottom) {
-//       nav.find('a').removeClass('active');
-//       sections.removeClass('active');
-
-//       $(this).addClass('active');
-//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-//     }
-//   });
-// });
-
-
-
-
-
-
-
-
+$('section').mouseenter(function(){
+     var id = $(this).attr('id');
+     console.log(id);
+     $('nav.a').removeClass('active');
+     const nav = $('<a href=#'+ id + '</a>');
+     console.log(nav);
+     $('a' + id).addClass('active');
+   });
 
 
 });
